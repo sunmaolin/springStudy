2c0ee71fb6438295a1a676b03d700a802459be8b
sunmaolin <Mr.sun727@iCloud.com>
2021-02-01 00:06:55 +0800

    springBean装配 装配的混合使用 1.配置类中引入xml装配的bean(@ImportResource) 2.配置类中引入其他配置类(@Import) 3.xml引入其他xml(<import>) 4.xml中定义扫描包自动装配(<context:component-scan>)

--- src/main/java/annotation/config/ApplicationConfig.java
+++ src/main/java/annotation/config/ApplicationConfig.java
@@ -1,6 +1,8 @@
 package annotation.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 import pojo.Role;
 import service.RoleService;
 //扫描所定义的包
@@ -10,5 +12,11 @@ import service.RoleService;
 //或者同时定义
 //@ComponentScan(basePackages = {"pojo"},basePackageClasses = {Role.class})
 //注意：使用basePackages与basePackageClasses重复扫描不会导致重复创建 ， 而只是单纯的使用@Component会
+
+
+//将xml定义的Bean扫描进来
+//@ImportResource({"classpath:spring-config.xml"})
+//若我们有好多的配置类如ApplicationConfig1\ApplicationConfig2,可通过下面的注解
+//@Import({ApplicationConfig1.class})
 public class ApplicationConfig {
 }
